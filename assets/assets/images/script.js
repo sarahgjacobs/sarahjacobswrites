271037f444159f57495c4ce7fc156616965bc789
Sarah Jacobs <sgjacobs14@gmail.com>
2022-12-14 11:09:23 -0500

    added buttons with links

---
+++ assets/assets/images/script.js
@@ -0,0 +1,12 @@
+
+
+
+// const tcAmazon = document.getElementById("tcamazon")
+
+const goToAmazon () => {
+    document.getElementById('tcamazon').onclick = function () {
+        console.log('im working')
+        location.href = "https://www.amazon.com/Circuit-Sarah-Jacobs/dp/B0B2HN9P5G/ref=sr_1_1?crid=3N7K2T3T3EUBA&keywords=the+circuit+sarah+jacobs&qid=1671032851&sprefix=%2Caps%2C145&sr=8-1"
+    }
+    // tcAmazon.onclick(location.href = "https://www.amazon.com/Circuit-Sarah-Jacobs/dp/B0B2HN9P5G/ref=sr_1_1?crid=3N7K2T3T3EUBA&keywords=the+circuit+sarah+jacobs&qid=1671032851&sprefix=%2Caps%2C145&sr=8-1")
+}
